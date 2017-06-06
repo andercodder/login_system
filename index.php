bb8d5943d4dacc7493e3b573919922d3aa09b35b
andre <andre@debian>
2017-06-06 16:30:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+  <?php
+/*  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
+  ini_set('include_path', '/etc/php5/cli/20131226/pdo.so');
+
+  if (!defined('PDO::ATTR_DRIVER_NAME')) {
+  echo 'PDO unavailable';
+  }
+  elseif (defined('PDO::ATTR_DRIVER_NAME')) {
+  echo 'PDO available test'; }*/
+ ?>
+
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+<h2>User Authentication System </h2><hr>
+
+<?php
+//print_r(get_loaded_extensions());
+
+ include_once 'resource/Database.php';
+
+
+ ?>
+
+</body>
+</html>
---
+++ resource/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+//set_include_path('/etc/php5/cli/20131226/pdo.so');
+$username = 'andre';
+$dsn = 'mysql:host=localhost;dbname=register';
+$password = 'mysql.d3b1an1000!';
+
+
+try {
+  $db = new PDO($dsn, $username, $password);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  echo "conneted to register database";
+
+} catch (PDOException $ex) {
+  echo "Connetcion failed" . $ex->getMessage();
+
+}
+
+
+?>
