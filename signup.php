709659b3a9b6553973fe558976977280a1989da3
andre <andre@debian>
2017-06-07 16:58:54 +0100

    database connection is done

--- signup.php
+++ signup.php
@@ -1,4 +1,44 @@
-<!DOCTYPE html>
+
+<?php
+
+include_once 'resource/Database.php';
+if (isset($_POST['email'])) {
+  $email = $_POST['email'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+//HASHING PASSWORD
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+try {
+  //sql insert statement
+  $sqlInsert = "INSERT INTO users (username, email, password, join_date)
+                VALUES (:username, :email, :password, now())";
+  // add PDO prepared to sanitize data
+  $statement = $db->prepare($sqlInsert);
+
+  //add the data into the Database
+  $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+
+  //check if one new row was created
+   if ($statement->rowCount() == 1) {
+     $result = "<p style='padding:20px; color:green;'> Registration Sucessfully</p>";
+     # code...
+   }
+
+
+} catch (PDOException $ex) {
+  //display error message
+  $result = "<p style='padding:20px; color:red;'> An error occured: ".$ex->getMessage()." </p>";
+
+
+}
+
+}
+  //var_dump($_POST);
+
+ ?>
+ <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -9,16 +49,20 @@
     <h2>User Authentication System</h2><hr>
     <h3>Registration Form</h3>
 
+    <?php if (isset($result))  {
+      echo "$result";
+      # code...
+    } ?>
     <form class="" action="" method="post">
       <table>
         <tr>
-          <td>Email:</td> <td><input type="text" name="" value=""></td>
+          <td>Email:</td> <td><input type="text" name="email" value=""></td>
         </tr>
         <tr>
-          <td>Username:</td><td><input type="text" name="" value=""></td>
+          <td>Username:</td><td><input type="text" name="username" value=""></td>
         </tr>
         <tr>
-            <td>Password:</td><td><input type="text" name="" value=""></td>
+            <td>Password:</td><td><input type="text" name="password" value=""></td>
         </tr>
         <tr>
           <td><input  type="submit" name="" value="Signup" style="float:right;"></td>
