c5c3d9d173fa5923f80901681ca4648396952c11
andre <andre@debian>
2017-06-09 16:59:03 +0100

    Subject < 50 chars commeent aou lines in code Body in detail

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 
   <?php
-/*  error_reporting(E_ALL);
+ error_reporting(E_ALL);
   ini_set('display_errors', 1);
 
-  ini_set('include_path', '/etc/php5/cli/20131226/pdo.so');
+/*  ini_set('include_path', '/etc/php5/cli/20131226/pdo.so');
 
   if (!defined('PDO::ATTR_DRIVER_NAME')) {
   echo 'PDO unavailable';
@@ -21,7 +21,7 @@
 <body>
 <h2>User Authentication System </h2><hr>
 
-<p>you are not currently sign in <a href="login.php">login</a> Not yet a member? <a href="signup.php">Signup</a></p>
+<p>you are not currently sign in <a href="login.php">login</a> Not yet a member?<a href="signup.php">Signup</a></p>
 <p>you are logged in as {username} <a href="logout.php">Logout</a></p>
 
 <?php
--- signup.php
+++ signup.php
@@ -1,8 +1,41 @@
 
 <?php
 
+
+//error_reporting(E_ALL);
+ //ini_set('display_errors', 1);
+//add database connection script
 include_once 'resource/Database.php';
-if (isset($_POST['email'])) {
+
+//process the form
+if (isset($_POST['signupBtn'])) {
+
+  //initialize an array to store any error message from the form
+  $form_errors = array();
+ $name_of_fields = "";
+  //form validation
+  $required_fields = array('email', 'username', 'password');
+
+  //loop though the required fields ArrayAccess
+  foreach($required_fields as $name_of_fields) {
+
+    if (!isset($_POST[$name_of_fields]) || $_POST[$name_of_fields] == NULL) {
+      $form_errors[] = $name_of_fields . " is a required field ";
+      # code...
+    }
+
+  }
+  //check if error arry is empty, if yes process form data and insert resourcebundle_get_error_code
+
+
+    //collect form data and store in variable
+
+    # code...
+//  }
+//}
+
+if (empty($form_errors)) {
+  //collect form data and store in variable
   $email = $_POST['email'];
   $username = $_POST['username'];
   $password = $_POST['password'];
@@ -14,7 +47,7 @@ try {
   //sql insert statement
   $sqlInsert = "INSERT INTO users (username, email, password, join_date)
                 VALUES (:username, :email, :password, now())";
-  // add PDO prepared to sanitize data
+  // use PDO prepared to sanitize data
   $statement = $db->prepare($sqlInsert);
 
   //add the data into the Database
@@ -22,20 +55,38 @@ try {
 
   //check if one new row was created
    if ($statement->rowCount() == 1) {
-     $result = "<p style='padding:20px; color:green;'> Registration Sucessfully</p>";
+        $result = "<p style='padding:20px; color:green;'> Registration Sucessfully</p>";
      # code...
    }
-
-
 } catch (PDOException $ex) {
   //display error message
   $result = "<p style='padding:20px; color:red;'> An error occured: ".$ex->getMessage()." </p>";
+   }
+}else{
+  if (count($form_errors) == 1) {
+    $result = "<p style='color: red;'> There was 1 error in the form<br>";
+  //$result = "<p style='color:red;'> There were ".count($form_errors). "errors in the form <br>"
+  $result .= "<ul style='color:red;'>";
+  //loop though error array and display all imagecreatefromstring
+  foreach ($form_errors as $error) {
+      $result .= "<li> {$error} </li>";
+    }
+    $result .= "</ul></p>";
+  # code...
+}else {
+  $result = "<p style='color:red;'> There were " .count($form_errors). " errors in the form <br>";
 
-
-}
-
-}
-  //var_dump($_POST);
+  $result .= "<ul style='color:red;'>";
+  //loop though error array and displayy all imagecreatefromstring
+  foreach ($form_errors as $error) {
+    $result .= "<li>{$error}</li>";
+    # code...
+  }
+   $result .= "</ul></p>";
+   }
+  }
+ }
+//var_dump($_POST);
 
  ?>
  <!DOCTYPE html>
@@ -51,6 +102,7 @@ try {
 
     <?php if (isset($result))  {
       echo "$result";
+
       # code...
     } ?>
     <form class="" action="" method="post">
@@ -65,7 +117,7 @@ try {
             <td>Password:</td><td><input type="text" name="password" value=""></td>
         </tr>
         <tr>
-          <td><input  type="submit" name="" value="Signup" style="float:right;"></td>
+          <td ><input  type="submit" name="signupBtn" value="Signup" style="float:right;"></td>
         </tr>
       </table>
 
