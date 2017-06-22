66a9caece01419df66fdbf0d3a2d1533a0fea6bf
andercodder <andrevmc1000@hotmail.com>
2017-06-22 16:54:53 +0100

    logout is good at functions

--- logout.php
+++ logout.php
@@ -1,7 +1,9 @@
 <?php
 
     include_once 'resource/session.php';
+    include_once 'resource/utilities.php';
+
     session_destroy();
-    header('location: index.php');
+      redirectTo('index');
 
  ?>
